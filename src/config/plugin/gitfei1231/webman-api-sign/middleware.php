1757835be2873e8217280262cf4c3944681b4cd1
一男 <minzufeng@vip.qq.com>
2023-10-19 12:01:52 +0800

    修改默认中间件为API模式

--- src/config/plugin/gitfei1231/webman-api-sign/middleware.php
+++ src/config/plugin/gitfei1231/webman-api-sign/middleware.php
@@ -1,7 +1,14 @@
 <?php
+/*
+ * @Author: 一男 minzufeng@vip.qq.com
+ * @Date: 2023-10-19 11:51:23
+ * @LastEditors: 一男 minzufeng@vip.qq.com
+ * @LastEditTime: 2023-10-19 12:01:12
+ * @Description: 
+ */
 
 return [
-    '' => [
+    'api' => [
         \Wengg\WebmanApiSign\ApiSignMiddleware::class,
     ],
 ];
